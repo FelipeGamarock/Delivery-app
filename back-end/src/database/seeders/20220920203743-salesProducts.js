1ebd58e0f72e9b58d71b6793583304218a658523
LeonardoMonti <LeonardoMonti>
2022-09-20 19:48:09 -0300

    fix: change seeders

--- back-end/src/database/seeders/20220920203743-salesProducts.js
+++ back-end/src/database/seeders/20220920203743-salesProducts.js
@@ -1,20 +1,19 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-
-     await queryInterface.bulkInsert('SalesProducts', [{
-      "sale_id": 1,
-      "product_id": 1,
+  async up (queryInterface, _Sequelize) {
+    return queryInterface.bulkInsert('salesProducts', [{
+      sale_id: 1,
+      product_id: 1,
       quantity: 3,
      },{
-      "sale_id": 1,
-      "product_id": 2,
+      sale_id: 1,
+      product_id: 2,
       quantity: 5,
      }], {});
   },
 
-  async down (queryInterface, Sequelize) {
-      await queryInterface.bulkDelete('People', null, {});
+  async down (queryInterface, _Sequelize) {
+      return queryInterface.bulkDelete('salesProducts', null, {});
   }
 };
