bcc394c5266e1bf10fa3a4dd7b9d4b5da1fe5035
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-09-22 13:18:38 -0300

    refactor: route '/customer/product'

--- back-end/src/services/customer.js
+++ back-end/src/services/customer.js
@@ -2,15 +2,9 @@ const { Product } = require('../database/models');
 
 module.exports = {
   async products() {    
-    const product = await Product.findAll();
+    const resultProduct = await Product.findAll();
 
-    if (!product) return { status: 404, message: 'Products not found' };
-
-    const resultProduct = {
-      name: product.name,
-      price: product.price,
-      url: product.urlImage,
-    };
+    if (!resultProduct) return { status: 404, message: 'Products not found' };
 
     return { status: 200, resultProduct };
   },
