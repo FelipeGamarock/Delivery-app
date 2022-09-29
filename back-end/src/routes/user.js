ffee49767d57c78e73d8a188a699a719bf30cf5c
LeonardoMonti <LeonardoMonti>
2022-09-29 02:22:17 -0300

    feat(admin): route for create and delete user (backend)

--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const controller = require('../controllers/user');
+const validateToken = require('../middlewares/tokenJWT');
 
 const router = express.Router();
 
 router.get('/users', controller.findAll);
+
+router.delete('/users/:id', validateToken, controller.delete);
+
+router.post('/users', validateToken, controller.create);
 
 module.exports = router;
